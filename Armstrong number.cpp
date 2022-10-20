1d663dab5b189f60ea5306b060dcc73726254a6a
PrathIam <116181382+PrathIam@users.noreply.github.com>
2022-10-21 00:25:20 +0530

    Updated Code

--- Armstrong number.cpp
+++ Armstrong number.cpp
@@ -1,36 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int calc(int n, int power)
-{
-	int product=1;
-	for(int i=0;i<power;i++)
-	{
-	   product*=n;
-	}
-	return product;
-}
-int main()
-{
-	int i,count=0,rem,sum=0;
-	cout<<"Enter a number";
-	cin>>i;
-	int num=i;
-	//count digits in number
-	while(i>0)
-	{
-		i=1/10;
-		count++;
-	}
-	i=num;
-	while(i>0)
-	{
-		rem=i%10;
-		sum=sum+calc(rem,count);//calculate power of rem
-		i=i/10;
-	}
-	if(num==sum)
-		cout<<"\nArmstrong number!";
-	else
-		cout<<" \n Not an Armstrong Number!";
-	return 0;
+
+int main() {
+   int num, sum = 0, digit;
+   cout<<"Enter a positive integer: ";
+   cin>>num;
+
+   for(int temp=num; temp!=0;){
+      digit = temp % 10;
+      sum = sum +(digit * digit * digit);
+      temp = temp/10;
+   }
+
+   if(sum == num)
+      cout<<num<<" is an Armstrong number.";
+   else
+      cout<<num<<" is not an Armstrong number.";
+
+   return 0;
 }
